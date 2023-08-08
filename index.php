703bba72e08b3b06dfd0edf95f3dc824e7848c05
abhinavpb-249 <abhinavpb249@gmail.com>
2023-08-08 19:44:12 +0530

    initial

--- index.php
+++ index.php
@@ -2,36 +2,42 @@
 <html>
 <head>
     <title>Todo List App</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Todo List</h1>
 
-    <form action="add_task.php" method="POST">
-        <input type="text" name="task" placeholder="Enter a new task">
-        <button type="submit">Add Task</button>
-    </form>
 
-    <?php
-    // Display tasks from the database
-    $conn = new mysqli("localhost", "root", "abhinav", "new_database");
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+    <div class="main">
+        <h1>Todo List</h1>
+        <form action="add_task.php" method="POST">
+            <input type="text" name="task" placeholder="Enter a new task">
+            <button type="submit">Add Task</button>
+        </form>
+    
 
-    $sql = "SELECT * FROM tasks ORDER BY created_at DESC";
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        echo "<ul>";
-        while ($row = $result->fetch_assoc()) {
-            echo "<li>" . $row["task"] . "</li>";
+        <?php
+        // Display tasks from the database
+        $conn = new mysqli("localhost", "root", "abhinav", "new_database");
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
         }
-        echo "</ul>";
-    } else {
-        echo "No tasks yet.";
-    }
 
-    $conn->close();
-    ?>
+        $sql = "SELECT * FROM tasks ORDER BY created_at DESC";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "<ul>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<li>" . $row["task"] . "</li>";
+            }
+            echo "</ul>";
+        } else {
+            echo "No tasks yet.";
+        }
+
+        $conn->close();
+        ?>
+    </div>
+    
 </body>
 </html>
