1d3c382bbb13232ff1aaaced39eb9b99065119ca
abhinavpb-249 <abhinavpb249@gmail.com>
2023-08-10 10:55:43 +0530

    update

---
+++ upload.sql
@@ -0,0 +1,13 @@
+CREATE TABLE uploaded_data (
+    ->     id INT AUTO_INCREMENT PRIMARY KEY,
+    ->     name VARCHAR(100) NOT NULL,
+    ->     subject VARCHAR(100) NOT NULL,
+    ->     description TEXT NOT NULL,
+    ->     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    -> );
+
+ CREATE TABLE users (
+    ->     id INT AUTO_INCREMENT PRIMARY KEY,
+    ->     username VARCHAR(50) NOT NULL,
+    ->     password VARCHAR(255) NOT NULL
+    -> );
